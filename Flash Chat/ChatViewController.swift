23f033ffd2804425674fae1bd72981ec4e67bcfd
arshin <arshinx@users.noreply.github.com>
2018-12-18 23:32:30 -0800

    redirect to login screen upon logout

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -9,109 +9,106 @@ import Firebase
 
 class ChatViewController: UIViewController {
     
-    // Declare instance variables here
+   // Declare instance variables here
 
-    
-    // We've pre-linked the IBOutlets
-    @IBOutlet var heightConstraint: NSLayoutConstraint!
-    @IBOutlet var sendButton: UIButton!
-    @IBOutlet var messageTextfield: UITextField!
-    @IBOutlet var messageTableView: UITableView!
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        //TODO: Set yourself as the delegate and datasource here:
-        
-        
-        
-        //TODO: Set yourself as the delegate of the text field here:
 
-        
-        
-        //TODO: Set the tapGesture here:
-        
-        
+   // We've pre-linked the IBOutlets
+   @IBOutlet var heightConstraint: NSLayoutConstraint!
+   @IBOutlet var sendButton: UIButton!
+   @IBOutlet var messageTextfield: UITextField!
+   @IBOutlet var messageTableView: UITableView!
 
-        //TODO: Register your MessageCell.xib file here:
 
-        
-    }
 
-    ///////////////////////////////////////////
-    
-    //MARK: - TableView DataSource Methods
-    
-    
-    
-    //TODO: Declare cellForRowAtIndexPath here:
-    
-    
-    
-    //TODO: Declare numberOfRowsInSection here:
-    
-    
-    
-    //TODO: Declare tableViewTapped here:
-    
-    
-    
-    //TODO: Declare configureTableView here:
-    
-    
-    
-    ///////////////////////////////////////////
-    
-    //MARK:- TextField Delegate Methods
-    
-    
+   override func viewDidLoad() {
+     super.viewDidLoad()
+     
+     //TODO: Set yourself as the delegate and datasource here:
+     
+     
+     
+     //TODO: Set yourself as the delegate of the text field here:
 
-    
-    //TODO: Declare textFieldDidBeginEditing here:
-    
-    
-    
-    
-    //TODO: Declare textFieldDidEndEditing here:
-    
+     
+     
+     //TODO: Set the tapGesture here:
+     
+     
 
-    
-    ///////////////////////////////////////////
-    
-    
-    //MARK: - Send & Recieve from Firebase
-    
-    
-    
-    
-    
-    @IBAction func sendPressed(_ sender: AnyObject) {
-        
-        
-        //TODO: Send the message to Firebase and save it in our database
-        
-        
-    }
-    
-    //TODO: Create the retrieveMessages method here:
-    
-    
+     //TODO: Register your MessageCell.xib file here:
 
-    
-    
-    
-    @IBAction func logOutPressed(_ sender: AnyObject) {
-        
-        // Log out the user and send them back to WelcomeViewController
+     
+   }
+
+   ///////////////////////////////////////////
+
+   //MARK: - TableView DataSource Methods
+
+
+
+   //TODO: Declare cellForRowAtIndexPath here:
+
+
+
+   //TODO: Declare numberOfRowsInSection here:
+
+
+
+   //TODO: Declare tableViewTapped here:
+
+
+
+   //TODO: Declare configureTableView here:
+
+
+
+   ///////////////////////////////////////////
+
+   //MARK:- TextField Delegate Methods
+
+
+
+
+   //TODO: Declare textFieldDidBeginEditing here:
+
+
+
+
+   //TODO: Declare textFieldDidEndEditing here:
+
+
+
+   ///////////////////////////////////////////
+
+
+   //MARK: - Send & Recieve from Firebase
+
+
+
+
+
+   @IBAction func sendPressed(_ sender: AnyObject) {
+     
+     
+      //TODO: Send the message to Firebase and save it in our database
+     
+     
+   }
+
+   //TODO: Create the retrieveMessages method here:
+
+   
+   @IBAction func logOutPressed(_ sender: AnyObject) {
+     
+      // Log out the user and send them back to WelcomeViewController
       do {
          try Auth.auth().signOut()
+         navigationController?.popViewController(animated: true)
       } catch {
          print("Error Signing out!")
       }
-        
-    }
+     
+   }
     
 
 
