38d359de2c042881e99a9b05d40e3c0eeb487c82
arshin <arshinx@users.noreply.github.com>
2018-12-18 23:12:52 -0800

    User registration

--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -11,28 +11,38 @@ import Firebase
 
 class RegisterViewController: UIViewController {
 
-    
-    //Pre-linked IBOutlets
 
-    @IBOutlet var emailTextfield: UITextField!
-    @IBOutlet var passwordTextfield: UITextField!
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+   //Pre-linked IBOutlets
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    
+   @IBOutlet var emailTextfield: UITextField!
+   @IBOutlet var passwordTextfield: UITextField!
 
-  
-    @IBAction func registerPressed(_ sender: AnyObject) {
-        
-        // TODO: Set up a new user on our Firbase database
-        
-    } 
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+   }
+
+
+
+   @IBAction func registerPressed(_ sender: AnyObject) {
+     
+      // TODO: Set up a new user on our Firbase database
+      Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+         
+         if error != nil {
+            print(error!)
+         }
+         else {
+            print("Registration Success! \(self.emailTextfield.text!)")
+            self.performSegue(withIdentifier: "goToChat", sender: self)
+         }
+      }
+     
+   }
     
     
 }
